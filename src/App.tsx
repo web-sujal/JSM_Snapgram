d47fd5a4c30837d5982930628552f922bc5f53e9
Sujal <web.sujal@gmail.com>
2023-12-24 14:25:54 +0530

    added all components and created there routes

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,20 @@
 import { Route, Routes } from "react-router-dom";
 import SigninForm from "./_auth/Forms/SigninForm";
 import SignupForm from "./_auth/Forms/SignupForm";
-import { Home } from "./_root/pages";
+import {
+  AllUsers,
+  EditPost,
+  Explore,
+  Home,
+  PostDetails,
+  Profile,
+  Saved,
+  UpdateProfile,
+} from "./_root/pages";
 import AuthLayout from "./_auth/AuthLayout";
 import RootLayout from "./_root/RootLayout";
 import { Toaster } from "@/components/ui/toaster";
+import CreatePost from "./_root/pages/CreatePost";
 
 function App() {
   return (
@@ -19,6 +29,14 @@ function App() {
         {/* Private Routes */}
         <Route element={<RootLayout />}>
           <Route index element={<Home />} />
+          <Route path="/explore" element={<Explore />} />
+          <Route path="/saved" element={<Saved />} />
+          <Route path="/all-users" element={<AllUsers />} />
+          <Route path="/create-post" element={<CreatePost />} />
+          <Route path="/update-post/:id" element={<EditPost />} />
+          <Route path="/posts/:id" element={<PostDetails />} />
+          <Route path="/profile/:id/*" element={<Profile />} />
+          <Route path="/update-profile/:id" element={<UpdateProfile />} />
         </Route>
       </Routes>
 
