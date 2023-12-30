068f2858a28abb58b96b6a5ad76ba546a2e8480d
Sujal <web.sujal@gmail.com>
2023-12-30 19:46:22 +0530

    added GridPostList to Explore page for showing latest fetched posts
    - added useDebounce hook
    - created searchPosts and getInfinitePosts api
    - added searchedPosts functionality

--- src/lib/appwrite/api.ts
+++ src/lib/appwrite/api.ts
@@ -332,3 +332,41 @@ export async function deletePost(postId: string, imageId: string) {
     console.error(error);
   }
 }
+
+export async function getInfinitePosts({ pageParam }: { pageParam: number }) {
+  const queries: any[] = [Query.orderDesc("$updatedAt"), Query.limit(10)];
+
+  if (pageParam) {
+    queries.push(Query.cursorAfter(pageParam.toString()));
+  }
+
+  try {
+    const posts = await databases.listDocuments(
+      appwriteConfig.databaseId,
+      appwriteConfig.postsCollectionId,
+      queries
+    );
+
+    if (!posts) throw Error;
+
+    return posts;
+  } catch (error) {
+    console.error(error);
+  }
+}
+
+export async function searchPosts(searchTerm: string) {
+  try {
+    const posts = await databases.listDocuments(
+      appwriteConfig.databaseId,
+      appwriteConfig.postsCollectionId,
+      [Query.search("caption", searchTerm)]
+    );
+
+    if (!posts) throw Error;
+
+    return posts;
+  } catch (error) {
+    console.error(error);
+  }
+}
